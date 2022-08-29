464e5a4c9a38b393404bf5c9eb8467d0c832cdc4
PuhP <PuhP@lecture-vm.home>
2022-08-29 05:04:43 -0700

    Upgrade main.asm to print a message

--- 01 - First Program (Pure Assembly)/main.asm
+++ 01 - First Program (Pure Assembly)/main.asm
@@ -4,6 +4,21 @@
 ; Data section is for our global vars in system ram
 section .data
 
+;;;;;
+; System calls
+SYS_WRITE		equ		1
+
+;;;;;
+; File descriptors
+FD_STDOUT		equ		1
+
+
+
+;;;;;
+; Strings
+HELLO_MESSAGE		db		"Hello, my name is Gibsin Montgomery-Gibson !!",13,10
+HELLO_MESSAGE_LEN	equ		$-HELLO_MESSAGE
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Text section is for our instructions
@@ -14,8 +29,17 @@ section .text
 global _start
 _start:
 
-	nop	; Do nothing
-	nop	; Do nothing
-	nop	; Do nothing
+	;;;;;;;;;;;;;;;;;;;;
+	; Print out our hello message with a system call
+	mov rax, SYS_WRITE		; System call code goes into rax
+	mov rdi, FD_STDOUT		; Tell the system to print to STDOUT
+	mov rsi, HELLO_MESSAGE		; Provide the memory location to start reading our characters to print
+	mov rdx, HELLO_MESSAGE_LEN	; Provide the number of characters print
+	syscall				; Now that we have everything setup, actually do the system call
+	
+	
+
+
+
 
 
