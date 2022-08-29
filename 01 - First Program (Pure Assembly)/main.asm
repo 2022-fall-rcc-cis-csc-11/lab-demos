0b964c1d6bdb9b4e7131e6a524169c624e2f7396
PuhP <PuhP@lecture-vm.home>
2022-08-29 04:39:06 -0700

    Main program now compiles

--- 01 - First Program (Pure Assembly)/main.asm
+++ 01 - First Program (Pure Assembly)/main.asm
@@ -1,5 +1,21 @@
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Data section is for our global vars in system ram
+section .data
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Text section is for our instructions
+section .text
+
+
+;; The _start label is the entry point for our pure assembly program
+global _start
+_start:
+
+	nop	; Do nothing
+	nop	; Do nothing
+	nop	; Do nothing
 
 
